6b067028279b484b20e487290eaed12028ab6115
jnfran92 <jnfran92@gmail.com>
2020-12-18 20:05:51 -0300

    working on MVI

--- app/src/main/java/com/jnfran92/kotcoin/presentation/crypto/CryptoListViewModel.kt
+++ app/src/main/java/com/jnfran92/kotcoin/presentation/crypto/CryptoListViewModel.kt
@@ -3,6 +3,7 @@ package com.jnfran92.kotcoin.presentation.crypto
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.jnfran92.domain.crypto.usecase.GetCryptoListUseCase
+import com.jnfran92.kotcoin.KotcoinApp
 import com.jnfran92.kotcoin.presentation.crypto.action.CryptoListAction
 import com.jnfran92.kotcoin.presentation.crypto.intent.CryptoListIntent
 import com.jnfran92.kotcoin.presentation.crypto.mapper.DomainCryptoToUIMapper
@@ -10,10 +11,12 @@ import com.jnfran92.kotcoin.presentation.crypto.processor.CryptoListProcessor
 import com.jnfran92.kotcoin.presentation.crypto.result.CryptoListResult
 import com.jnfran92.kotcoin.presentation.crypto.uistate.CryptoListUIState
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.subjects.PublishSubject
+import timber.log.Timber
 import javax.inject.Inject
 
 class CryptoListViewModel(application: Application): AndroidViewModel(application) {
@@ -21,68 +24,50 @@ class CryptoListViewModel(application: Application): AndroidViewModel(applicatio
     @Inject lateinit var getCryptoListUseCase: GetCryptoListUseCase
     @Inject lateinit var mapper: DomainCryptoToUIMapper
 
-//    @Inject lateinit var interpreter: CryptoListInterpreter
-
-//
     private val compositeDisposable = CompositeDisposable()
-//
-//
-//    private val interpreter: PublishSubject<CryptoListIntent> = PublishSubject.create()
-//
-//
-////    private fun CryptoListIntent.toAction(): CryptoListAction{
-////        return when(this){
-////            CryptoListIntent.getCryptoListIntent -> {CryptoListAction.getCryptoList}
-////            is CryptoListIntent.reloadCryptoList -> {CryptoListAction.noAvailableAction}
-////            is CryptoListIntent.updateCryptoItemIntent -> {CryptoListAction.noAvailableAction}
-////            is CryptoListIntent.getCryptoItemDetailsIntent -> {CryptoListAction.getCryptoItemDetails(itemId)}
-////        }
-////    }
-//
-//    private fun CryptoListResult.toUIState() {
-//        Timber.d("toUIState")
-//        return when(this){
-//            else -> {}
-//        }
-//    }
-//
-//
-//
-//    init {
-//        val applicationComponent = (application as KotcoinApp).applicationComponent
-//        applicationComponent.inject(this)
-//
-//
-////        val interpreterMapped = interpreter
 
 
+    lateinit var publishSubject: PublishSubject<CryptoListIntent>
+    lateinit var interpreter: Observable<CryptoListAction>
+    lateinit var processor: CryptoListProcessor
+    lateinit var reducer: Observable<CryptoListUIState>
 
-    private val publishSubject: PublishSubject<CryptoListIntent> = PublishSubject.create()
-    private val interpreter = publishSubject.flatMap {
-        Observable.create<CryptoListAction> { emitter ->
-            when(it){
-                CryptoListIntent.getCryptoListIntent -> {emitter.onNext(CryptoListAction.getCryptoList)}
-                is CryptoListIntent.reloadCryptoList -> {}
-                is CryptoListIntent.updateCryptoItemIntent -> {}
-                is CryptoListIntent.getCryptoItemDetailsIntent -> {}
-            }
-        }
-    }
-
-    private val processor = CryptoListProcessor(getCryptoListUseCase, mapper)
-
-    private val reducer = processor.tx.flatMap { result ->
-        Observable.create<CryptoListUIState> { emitter ->
-            when(result){
-                CryptoListResult.GetCryptoListResult.InProgress -> { emitter.onNext(CryptoListUIState.ShowLoadingView)}
-                is CryptoListResult.GetCryptoListResult.OnError -> {emitter.onNext(CryptoListUIState.ShowLoadingView)}
-                is CryptoListResult.GetCryptoListResult.OnSuccess -> {emitter.onNext(CryptoListUIState.ShowLoadingView)}
-            }
-        }
-    }
 
     init {
-        this.compositeDisposable += interpreter.subscribeWith(processor.rx)
+        val applicationComponent = (application as KotcoinApp).applicationComponent
+        applicationComponent.inject(this)
+
+
+
+        publishSubject = PublishSubject.create()
+        interpreter = publishSubject.flatMap {
+            Observable.create<CryptoListAction> { emitter ->
+                Timber.d("interpreter: intent $it")
+                when(it){
+                    CryptoListIntent.getCryptoListIntent -> {emitter.onNext(CryptoListAction.getCryptoList)}
+                    is CryptoListIntent.reloadCryptoList -> {}
+                    is CryptoListIntent.updateCryptoItemIntent -> {}
+                    is CryptoListIntent.getCryptoItemDetailsIntent -> {}
+                }
+            }
+        }
+        processor = CryptoListProcessor(getCryptoListUseCase, mapper)
+
+        reducer = processor.tx.flatMap { result ->
+            Observable.create<CryptoListUIState> { emitter ->
+                Timber.d("reducer: $result")
+                when(result){
+                    CryptoListResult.GetCryptoListResult.InProgress -> { emitter.onNext(CryptoListUIState.ShowLoadingView)}
+                    is CryptoListResult.GetCryptoListResult.OnError -> {emitter.onNext(CryptoListUIState.ShowErrorRetryView)}
+                    is CryptoListResult.GetCryptoListResult.OnSuccess -> {emitter.onNext(CryptoListUIState.ShowDataView(result.data))}
+                }
+            }
+        }
+
+
+
+        this.compositeDisposable += interpreter
+            .subscribeWith(processor.rx)
     }
 
     fun processIntent(intent: CryptoListIntent){
@@ -90,138 +75,11 @@ class CryptoListViewModel(application: Application): AndroidViewModel(applicatio
     }
 
     fun observeUIStates(observer: DisposableObserver<CryptoListUIState>){
-        this.compositeDisposable += reducer.subscribeWith(observer)
+        this.compositeDisposable += reducer
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(observer)
     }
-
-//
-////        val publisherR: PublishSubject<CryptoListResult> = PublishSubject.create()
-////        val reducer = publisherR.map { it.toUIState() }
-//
-//
-////        val processor = interpreter.flatMap {
-////            Observable<CryptoListAction>.create{}
-////        }
-//
-//
-//        val processor = interpreter.flatMap {action ->
-//            Observable.create<CryptoListResult>{ emitter ->
-//                when(action){
-//                    CryptoListAction.getCryptoList -> {
-//                        getCryptoListUseCase
-//                            .useCase
-//                            .toObservable()
-//                            .startWith{ emitter.onNext(CryptoListResult.GetCryptoListResult.InProgress) }
-//                            .doOnError { emitter.onNext(CryptoListResult.GetCryptoListResult.OnError(it)) }
-//                            .doOnNext { emitter.onNext(CryptoListResult.GetCryptoListResult.OnSuccess(with(mapper){it.toUIModel()})) }
-//                            .blockingFirst()
-//                    }
-//                    CryptoListAction.noAvailableAction -> {
-//
-//                    }
-//                    is CryptoListAction.getCryptoItemDetails -> {
-//
-//                    }
-//                }
-//            }
-//        }
-//
-//
-//        val a = interpreter.flatMap { action->
-//            when(action){
-//                CryptoListAction.getCryptoList -> {
-//                    getCryptoListUseCase
-//                        .useCase
-//                        .toObservable()
-//                        .map { CryptoListResult.GetCryptoListResult.OnSuccess(with(mapper){it.toUIModel()}) }
-//                        .cast(CryptoListResult::class.java)
-//                }
-//                CryptoListAction.noAvailableAction -> TODO()
-//                is CryptoListAction.getCryptoItemDetails -> TODO()
-//            }
-//        }
-//
-//        a
-//
-//
-//        val reducer = processor.flatMap { result ->
-//            Observable.create<CryptoListUIState> { emitter->
-//                when(result){
-//                    CryptoListResult.GetCryptoListResult.InProgress -> {
-//                        emitter.onNext(CryptoListUIState.ShowDefaultView)
-//                        emitter.onNext(CryptoListUIState.ShowLoadingView)
-//                    }
-//                    is CryptoListResult.GetCryptoListResult.OnError -> {
-//                        emitter.onNext(CryptoListUIState.ShowErrorRetryView)
-//                        emitter.onNext(CryptoListUIState.HideLoadingView)
-//                    }
-//                    is CryptoListResult.GetCryptoListResult.OnSuccess -> {
-//                        emitter.onNext(CryptoListUIState.ShowDataView(result.data))
-//                        emitter.onNext(CryptoListUIState.HideLoadingView)
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//
-////    val cryptoList: MutableLiveData<List<UICrypto>> by lazy{
-////        MutableLiveData<List<UICrypto>>().also { loadCryptoList() }
-////    }
-//
-//    /**
-//     * Tx: Transmit UI Events
-//     */
-//    val tx: MutableLiveData<List<CryptoListUIState>> by lazy {
-//        MutableLiveData<List<CryptoListUIState>>()
-//    }
-//
-//
-//    /**
-//     * Rx: Receive intents
-//     */
-//    fun rx(intent: CryptoListIntent){
-//        Timber.d("sendIntent: $intent")
-//        this.interpreter.processIntent(intent)
-//    }
-//
-//
-//    private fun getCryptoList(){
-//        Timber.d("getCryptoList: ")
-//        tx.postValue(listOf(
-//            CryptoListUIState.ShowDefaultView,
-//            CryptoListUIState.HideLoadingView,
-//            CryptoListUIState.HideErrorRetryView,
-//            CryptoListUIState.HideLoadingView,
-//            CryptoListUIState.HideDataView,
-//            CryptoListUIState.ShowLoadingView
-//        ))
-//
-//        this.getCryptoListUseCase.execute(object : DisposableSingleObserver<List<DomainCrypto>>() {
-//            override fun onSuccess(t: List<DomainCrypto>) {
-//                Timber.d("onSuccess $t")
-//                tx.postValue(listOf(
-//                    CryptoListUIState.HideDefaultView,
-//                    CryptoListUIState.HideLoadingView,
-//                    CryptoListUIState.HideErrorRetryView,
-//                    CryptoListUIState.HideLoadingView,
-//                    CryptoListUIState.ShowDataView(t.map { with(mapper){it.toUIModel()} }),
-//                    CryptoListUIState.HideLoadingView
-//                ))
-//            }
-//
-//            override fun onError(e: Throwable) {
-//                Timber.d("onError")
-//                tx.postValue(listOf(
-//                    CryptoListUIState.HideDefaultView,
-//                    CryptoListUIState.HideLoadingView,
-//                    CryptoListUIState.ShowErrorRetryView,
-//                    CryptoListUIState.HideLoadingView,
-//                    CryptoListUIState.HideDataView,
-//                    CryptoListUIState.HideLoadingView
-//                ))
-//            }
-//        })
-//    }
 
 
     override fun onCleared() {
--- app/src/main/java/com/jnfran92/kotcoin/ui/crypto/fragment/CryptoListFragment.kt
+++ app/src/main/java/com/jnfran92/kotcoin/ui/crypto/fragment/CryptoListFragment.kt
@@ -15,6 +15,7 @@ import com.jnfran92.kotcoin.presentation.crypto.model.UICrypto
 import com.jnfran92.kotcoin.presentation.crypto.uistate.CryptoListUIState
 import com.jnfran92.kotcoin.ui.crypto.activity.CryptoActivity
 import com.jnfran92.kotcoin.ui.crypto.adapter.CryptoListAdapter
+import io.reactivex.observers.DisposableObserver
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -68,36 +69,84 @@ class CryptoListFragment : Fragment() {
 //            this.cryptoListAdapter.setData(it)
 //        }
 
-        this.viewModel.tx.observe(viewLifecycleOwner){
-            Timber.d("initViewModel: $it")
-            it.forEach { cryptoListUIState ->
-                Timber.d("initViewModel: forEach $cryptoListUIState")
-                when(cryptoListUIState){
-                    CryptoListUIState.ShowDefaultView -> {this.cryptoListAdapter.setData(arrayListOf(
-                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
-                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
-                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
-                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
-                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
-                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
-                    ))}
-                    CryptoListUIState.HideDefaultView -> {}
-                    CryptoListUIState.ShowLoadingView -> {binding.pbCryptoFragmentLoading.pbViewLoadingLoading.visibility = View.VISIBLE}
-                    CryptoListUIState.HideLoadingView -> {binding.pbCryptoFragmentLoading.pbViewLoadingLoading.visibility = View.INVISIBLE}
-                    CryptoListUIState.ShowErrorRetryView -> {}
-                    CryptoListUIState.HideErrorRetryView -> {}
-                    is CryptoListUIState.ShowDataView -> {this.cryptoListAdapter.setData(cryptoListUIState.data)}
-                    CryptoListUIState.HideDataView -> {}
-                    is CryptoListUIState.OnShowToastMessage -> {}
+//        this.viewModel.tx.observe(viewLifecycleOwner){
+//            Timber.d("initViewModel: $it")
+//            it.forEach { cryptoListUIState ->
+//                Timber.d("initViewModel: forEach $cryptoListUIState")
+//                when(cryptoListUIState){
+//                    CryptoListUIState.ShowDefaultView -> {this.cryptoListAdapter.setData(arrayListOf(
+//                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+//                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+//                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+//                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+//                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+//                        UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+//                    ))}
+//                    CryptoListUIState.HideDefaultView -> {}
+//                    CryptoListUIState.ShowLoadingView -> {binding.pbCryptoFragmentLoading.pbViewLoadingLoading.visibility = View.VISIBLE}
+//                    CryptoListUIState.HideLoadingView -> {binding.pbCryptoFragmentLoading.pbViewLoadingLoading.visibility = View.INVISIBLE}
+//                    CryptoListUIState.ShowErrorRetryView -> {}
+//                    CryptoListUIState.HideErrorRetryView -> {}
+//                    is CryptoListUIState.ShowDataView -> {this.cryptoListAdapter.setData(cryptoListUIState.data)}
+//                    CryptoListUIState.HideDataView -> {}
+//                    is CryptoListUIState.OnShowToastMessage -> {}
+//                }
+//            }
+//        }
+
+        this.viewModel.observeUIStates(object : DisposableObserver<CryptoListUIState>() {
+            override fun onNext(t: CryptoListUIState) {
+                Timber.d("onNext $t")
+                when (t) {
+                    CryptoListUIState.ShowDefaultView -> {
+                        cryptoListAdapter.setData(
+                            arrayListOf(
+                                UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+                                UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+                                UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+                                UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+                                UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+                                UICrypto(-1, "", "-", "", 0.0, 0.0, ""),
+                            )
+                        )
+                    }
+
+                    CryptoListUIState.ShowLoadingView -> {
+                        binding.pbCryptoFragmentLoading.pbViewLoadingLoading.visibility =
+                            View.VISIBLE
+                    }
+                    CryptoListUIState.HideLoadingView -> {
+                        binding.pbCryptoFragmentLoading.pbViewLoadingLoading.visibility =
+                            View.INVISIBLE
+                    }
+
+                    CryptoListUIState.ShowErrorRetryView -> {
+                    }
+                    is CryptoListUIState.ShowDataView -> {
+                        cryptoListAdapter.setData(t.data)
+                    }
+                    is CryptoListUIState.OnShowToastMessage -> {
+                    }
                 }
             }
-        }
+
+            override fun onError(e: Throwable) {
+                Timber.d("onError")
+            }
+
+            override fun onComplete() {
+                Timber.d("onComplete")
+            }
+
+        })
+
     }
 
 
     override fun onResume() {
         super.onResume()
         Timber.d("onResume: ")
-        viewModel.rx(CryptoListIntent.getCryptoListIntent)
+//        viewModel.rx(CryptoListIntent.getCryptoListIntent)
+        viewModel.processIntent(CryptoListIntent.getCryptoListIntent)
     }
 }
